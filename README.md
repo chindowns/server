ba51823df44ad73d404a8e2533fedbacb0f23c5e
ChinDowns <webdev@chindowns.com>
2020-08-28 10:13:17 -0700

    [Add] MySQL / Sequelize configuration

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# server
+# Chindowns.app Server
+
+This will be the server for many of the ChinDowns Apps.  The server is running Node.js with a MySQL database.
---
+++ models/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV;
+const config = require('../config/config.js')[env];
+const db = {};
+
+if (config.use_env_variable) {
+    var sequelize = new Sequelize(process.env[config.use_env_variable], { dialect: 'mysql', operatorsAliases: false });
+} else {
+    var sequelize = new Sequelize(config.database, config.username, config.password, { host: config.host, dialect: 'mysql', operatorsAliases: false });
+}
+
+fs
+    .readdirSync(__dirname)
+    .filter(file => {
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    })
+    .forEach(file => {
+        const model = sequelize['import'](path.join(__dirname, file));
+        db[model.name] = model;
+    });
+
+Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+exports.db = db;
